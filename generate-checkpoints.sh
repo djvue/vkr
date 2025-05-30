214c280ba5f10a95a1b11f90d2a9f2ac0985b0fa
Daniil Vakhlov <d.vahlov@yandex.ru>
2025-05-30 21:25:59 +0300

    upd train analyzing

--- generate-checkpoints.sh
+++ generate-checkpoints.sh
@@ -2,5 +2,8 @@
 
 for number in 100 200 300 400 500 600 700 800 900 1000 1100 1200 1300 1400 1500
 do
-    python eval.py score checkpoints_model_12/checkpoint-$number 300 --no-fixing
+    echo "starting generate checkpoint $number"
+    python eval.py generate checkpoints_model_12/checkpoint-$number 300 --no-fixing
+    echo "finished generate checkpoint $number"
+    echo ""
 done
--- score-checkpoints.sh
+++ score-checkpoints.sh
@@ -2,5 +2,10 @@
 
 for number in 100 200 300 400 500 600 700 800 900 1000 1100 1200 1300 1400 1500
 do
+    echo ""
+    echo "starting score checkpoint $number"
     python eval.py score checkpoints_model_12/checkpoint-$number 300 --no-fixing
+    echo "finished score checkpoint $number"
+    echo ""
+    echo ""
 done
